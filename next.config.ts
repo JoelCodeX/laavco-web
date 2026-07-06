8ebd0fbf8bd88764aa408e3d2dc72ab5dcaf4812
𝓳𝓸𝓮𝓵_𝓭𝓮𝓿 <joelr20011@gmail.com>
2026-07-06 04:51:38 -0500

    Configure GitHub Pages deployment via GitHub Actions

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export",
+  basePath: "/laavco-web",
+  images: {
+    unoptimized: true,
+  },
   allowedDevOrigins: ["192.168.0.15"]
 };
 
