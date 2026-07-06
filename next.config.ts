a66c150a8feb43ce66ca6ece8497464922fb5a6d
𝓳𝓸𝓮𝓵_𝓭𝓮𝓿 <joelr20011@gmail.com>
2026-07-06 05:03:42 -0500

    Fix submenus routing on GitHub Pages and make config compatible with Vercel

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,14 @@
 import type { NextConfig } from "next";
 
+const isGithubActions = process.env.GITHUB_ACTIONS === "true";
+
 const nextConfig: NextConfig = {
-  output: "export",
-  basePath: "/laavco-web",
+  output: isGithubActions ? "export" : undefined,
+  basePath: isGithubActions ? "/laavco-web" : undefined,
   images: {
-    unoptimized: true,
+    unoptimized: isGithubActions ? true : undefined,
   },
+  trailingSlash: true,
   allowedDevOrigins: ["192.168.0.15"]
 };
 
